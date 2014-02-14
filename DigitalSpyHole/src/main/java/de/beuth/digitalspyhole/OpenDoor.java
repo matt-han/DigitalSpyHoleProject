564b369fbd6b2bfbbc30207a432616da97fe62ba
nik87 <partylunzer@partyheld.de>
2014-02-14 13:23:13 +0100

    http request realisiert

--- DigitalSpyHole/src/main/java/de/beuth/digitalspyhole/OpenDoor.java
+++ DigitalSpyHole/src/main/java/de/beuth/digitalspyhole/OpenDoor.java
@@ -1,5 +1,9 @@
 package de.beuth.digitalspyhole;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 /**
  * Created by Niklas Knauer on 30.10.13.
  */
@@ -7,6 +11,13 @@ public class OpenDoor {
 
 
     public OpenDoor() {
-
+        try{
+            URL u = new URL("http://spyhole.no-ip.biz?open=open");
+            u.openStream();
+        }catch(MalformedURLException e){
+            e.printStackTrace();
+        }catch(IOException e){
+            e.printStackTrace();
+        }
     }
 }
